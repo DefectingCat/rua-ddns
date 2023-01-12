4a64ec00e5941ec6ec4c4fe88fe1944a39f3ef11
DefectingCat <i@defect.ink>
2023-01-12 10:05:37 +0800

    Add config file path

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-console.log('hello');
+import './utils/config';
---
+++ src/utils/config.ts
@@ -0,0 +1,7 @@
+import fs from 'fs';
+import path from 'path';
+
+const filename = 'config.json';
+const filePath = path.resolve('./', filename);
+
+console.log(filePath);
